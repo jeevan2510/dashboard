458ac8eb28f54fa04c83b75ae1bdbf25bf2e1ec1
jeevan2510 <jeevanrajuchonedan6@gmail.com>
2023-01-04 11:45:49 +0530

    dashboard

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { outputAst } from '@angular/compiler';
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Output()
+  isSidebarToggle = new EventEmitter()
+  sideBarToggle() {
+    this.isSidebarToggle.emit()
+    setTimeout(()=>{
+    window.dispatchEvent(
+      new Event('resize')
+    )},300);
+  }
+}
